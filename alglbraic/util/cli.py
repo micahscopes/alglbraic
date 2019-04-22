6d813b7fdc4c7153d8ae9f4788585d6dc8a405e6
Micah Fitch <fitchmicah@gmail.com>
2019-04-22 02:43:47 -0700

    formatting

--- alglbraic/util/cli.py
+++ alglbraic/util/cli.py
@@ -79,7 +79,7 @@ class GroupWithCommandOptions(click.Group):
 @click.pass_context
 def cli(ctx, out, glslify, **opts):
     ctx.obj["out"] = out
-    ctx.obj["glslify"] = glslify 
+    ctx.obj["glslify"] = glslify
 
 
 @cli.resultcallback()
@@ -99,11 +99,13 @@ def process_result(obj, *args, **opts):
             os.makedirs(out)
         os.chdir(out)
         if glslify:
-            os.remove('index.glsl')
-            index = open('index.glsl', 'a+')
+            os.remove("index.glsl")
+            index = open("index.glsl", "a+")
         for (name, bundle) in bundles.items():
             if glslify:
-                index.write(f"// {name}.glsl\n#pragma glslify: import('./{name}.glsl')\n\n")
+                index.write(
+                    f"// {name}.glsl\n#pragma glslify: import('./{name}.glsl')\n\n"
+                )
             f = open(f"{name}.glsl", "w")
             f.write(bundle)
             f.close()
@@ -118,49 +120,4 @@ if __name__ == "__main__":
     from collections import OrderedDict
 
     cli(obj={"results": OrderedDict()})
-
-
-# class ConfigSectionSchema(object):
-
-#     @matches_section("foo")
-#     class Foo(SectionSchema):
-#         name    = Param(type=str)
-#         flag    = Param(type=bool, default=True)
-#         numbers = Param(type=int, multiple=True)
-#         filenames = Param(type=click.Path(), multiple=True)
-
-#     @matches_section("person.*")   # Matches multiple sections
-#     class Person(SectionSchema):
-#         name      = Param(type=str)
-#         birthyear = Param(type=click.IntRange(1990, 2100))
-
-
-# class ConfigFileProcessor(ConfigFileReader):
-#     config_files = ["alglbraic.ini", "alglbraic.cfg"]
-#     # config_section_schemas = []
-
-
-# # -- COMMAND:
-# CONTEXT_SETTINGS = dict(default_map=ConfigFileProcessor.read_config())
-
-
-# @click.command(context_settings=CONTEXT_SETTINGS)
-# # @click.option("-n", "--number", "numbers", type=int, multiple=True)
-# @click.pass_context
-# def command_with_config(ctx, numbers):
-#     pass
-#     # -- ACCESS ADDITIONAL DATA FROM CONFIG FILES: Using ctx.default_map
-#     # for person_data_key in ctx.default_map.keys():
-#     #     if not person_data_key.startswith("person."):
-#     #         continue
-#     #     person_data = ctx.default_map[person_data_key]
-#     #     process_person_data(person_data)    # as dict.
-
-
-# # @click.group()
-# # @click.option('--debug/--no-debug', default=False)
-# # def cli(debug):
-# #     click.echo('Debug mode is %s' % ('on' if debug else 'off'))
-
-# # import click
 
